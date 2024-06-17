241d7cf5ae9a50dd279f52f3d8703bfe765726dd
Dubemxe <dubemxe@gmail.com>
2024-06-17 08:46:27 +0000

    error handled

--- markdown2html.py
+++ markdown2html.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-
+'''
+A markdown2html script
+'''
 import os
 import sys
 
 
 def main():
+    """Checks if the script takes only 2 arguments."""
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html",
               file=sys.stderr)
@@ -13,6 +16,7 @@ def main():
     input_file = sys.argv[1]
     output_file = sys.argv[2]
 
+    """Checks if the input file exists"""
     if not os.path.isfile(input_file):
         print(f"Missing {input_file}", file=sys.stderr)
         sys.exit(1)
